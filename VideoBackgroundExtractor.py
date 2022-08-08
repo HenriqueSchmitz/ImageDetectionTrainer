7c0df731222810bf6440a18fe848bc3e86cb631f
Henrique Schmitz <schhenrique@gmail.com>
2022-08-08 19:07:47 -0300

    Video Background Extractor (Optimized for GPU)

---
+++ VideoBackgroundExtractor.py
@@ -0,0 +1,69 @@
+import numpy as np
+import torch
+import cv2
+
+class VideoBackgroundExtractor:
+  def __init__(self):
+    self.__backgroundTensor = None;
+    self.__isGpuAvailable = torch.cuda.is_available();
+
+  def loadVideo(self, video, numberOfFramesToUse = 25):
+    frames = self.__getRandomFramesFromVideo(video, numberOfFramesToUse)
+    framesTensor = torch.from_numpy(np.asarray(frames))
+    if self.__isGpuAvailable:
+      framesTensor = framesTensor.cuda()
+    median = torch.median(framesTensor, dim = 0)
+    self.__backgroundTensor = median.values
+
+  def __getRandomFramesFromVideo(self, video, numberOfFramesToUse):
+    previousPosition = video.get(cv2.CAP_PROP_POS_FRAMES)
+    frameIds = torch.mul(torch.rand(numberOfFramesToUse), (video.get(cv2.CAP_PROP_FRAME_COUNT)))
+    frames = []
+    for fid in frameIds:
+        video.set(cv2.CAP_PROP_POS_FRAMES, fid.item())
+        ret, frame = video.read()
+        frames.append(frame)
+    video.set(cv2.CAP_PROP_POS_FRAMES, previousPosition)
+    return frames
+
+  def getBackgroundFrame(self):
+    self.__validadeBackground()
+    if self.__isGpuAvailable:
+      return self.__backgroundTensor.cpu().numpy()
+    return self.__backgroundTensor.numpy()
+  
+  def __validadeBackground(self):
+    if self.__backgroundTensor == None:
+      raise Exception("No video loaded to get background from")
+
+  def getDifferenceToBackground(self, image):
+    self.__validadeBackground()
+    if self.__isGpuAvailable:
+      return self.__getDifferenceToBackgroundGpu(image)
+    else:
+      return self.__getDifferenceToBackgroundCpu(image)
+
+  def __getDifferenceToBackgroundGpu(self, image):
+      imageTensor = torch.from_numpy(image).cuda().to(torch.int16)
+      differenceTensor = self.__getDifferenceTensor(imageTensor)
+      return differenceTensor.cpu().numpy()
+
+  def __getDifferenceToBackgroundCpu(self, image):
+      imageTensor = torch.from_numpy(image).to(torch.int16)
+      differenceTensor = self.__getDifferenceTensor(imageTensor)
+      return addedFrame.numpy()
+
+  def __getDifferenceTensor(self, imageTensor):
+      differencePerColorTensor = torch.abs(torch.subtract(imageTensor, self.__backgroundTensor))
+      averageDifferenceTensor = torch.div(torch.sum(differencePerColorTensor, dim=2), 3)
+      differenceTensor = averageDifferenceTensor.to(torch.uint8)
+      return differenceTensor
+
+  def getDifferenceIndex(self, image):
+    self.__validadeBackground()
+    imageTensor = torch.from_numpy(image)
+    if self.__isGpuAvailable:
+      imageTensor = imageTensor.cuda()
+    differenceTensor = self.__getDifferenceTensor(imageTensor.to(torch.int16))
+    differenceMean = torch.div(torch.mean(differenceTensor.double()), 255)
+    return differenceMean.item()
