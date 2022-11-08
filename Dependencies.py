e84aa7656277f348d050c69810f239c7c0db5123
Henrique Schmitz <henrique.schmitz@sap.com>
2022-11-08 19:38:46 -0300

    Changed terminal calls to use subprocess

--- Dependencies.py
+++ Dependencies.py
@@ -1,13 +1,18 @@
+def runShellCommand(command):
+    import subprocess
+    return subprocess.check_output(command, shell=True).decode("utf-8").split("\n")
+
 def prepareDependencies():
     import os
     import glob
     import xml.etree.ElementTree as ET
     import pandas as pd
     import tensorflow as tf
+    import subprocess
     print(tf.__version__)
 
     # Check GPU version
-    gpu_info = ! nvidia-smi
+    gpu_info = runShellCommand("nvidia-smi")
     gpu_info = '\n'.join(gpu_info)
     if gpu_info.find('failed') >= 0:
         print('Not connected to a GPU')
@@ -17,21 +22,21 @@ def prepareDependencies():
     # Mount Drive
     from google.colab import drive
     drive.mount('/content/gdrive')
-    !ln -s /content/gdrive/My\ Drive/ /mydrive
+    runShellCommand("ln -s /content/gdrive/My\ Drive/ /mydrive")
 
     # Setup Tensorflow
-    !git clone --q https://github.com/tensorflow/models.git
-    %cd models/research
-    !protoc object_detection/protos/*.proto --python_out=.
-    !cp object_detection/packages/tf2/setup.py .
-    !python -m pip --q install .
+    runShellCommand("git clone --q https://github.com/tensorflow/models.git")
+    runShellCommand("cd models/research")
+    runShellCommand("protoc object_detection/protos/*.proto --python_out=.")
+    runShellCommand("cp object_detection/packages/tf2/setup.py .")
+    runShellCommand("python -m pip --q install .")
 
     # Correct opencv version
     import os
     desiredOpencvVersion = "4.1.2.30"
-    opencvVersions = !pip list|grep opencv
+    opencvVersions = runShellCommand("pip list|grep opencv")
     for opencvSubLibraryVersion in opencvVersions:
         if desiredOpencvVersion not in opencvSubLibraryVersion:
             libraryName = opencvSubLibraryVersion.split(" ")[0]
-            !pip uninstall $libraryName --y
-            !pip install $libraryName==$desiredOpencvVersion
+            runShellCommand("pip uninstall $libraryName --y")
+            runShellCommand("pip install $libraryName==$desiredOpencvVersion")
